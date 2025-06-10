edd691baf79ed8e6a9a79da55e46e2a95b2c4e22
Aadit <aadits2004@gmail.com>
2025-06-10 06:53:51 +0000

    room routes

--- apps/http-backend/src/room.ts
+++ apps/http-backend/src/room.ts
@@ -22,12 +22,92 @@ roomRouter.post("/room", async function (req: Request, res: Response) {
         return
     }
 
-    await prismaClient.room.create({
-        data: {
-            slug: parsedData.data.name,
-            admin: userId
+    try {
+        const room = await prismaClient.room.create({
+            data: {
+                slug: parsedData.data.name,
+                admin: {
+                    connect: {
+                        id: userId
+                    }
+                }
+            }
+        });
+
+        res.status(200).json({
+            message: "Room created successfully!!!",
+            roomId: room.id
+        })
+    } catch (e) {
+        res.status(403).json({
+            message: "Room with that name already exists.❌❌"
+        })
+    }
+
+})
+
+roomRouter.get("/shapes/:roomId", async function (req: Request, res: Response) {
+    const roomId = Number(req.params.roomId);
+
+    if (!roomId) {
+        console.log("No roomId provided.");
+        res.status(403).json({
+            message: "No roomId provided."
+        })
+        return
+    }
+
+    try {
+        const shapes = await prismaClient.shape.findMany({
+            where: {
+                roomId: roomId
+            },
+            take: 1000,
+            orderBy: {
+                id: "desc"
+            }
+        })
+        res.status(200).json({
+            shapes
+        })
+
+    } catch (e) {
+        res.status(403).json({
+            message: "Could not find the desired room."
+        })
+    }
+
+})
+
+roomRouter.get("/room/:slug", async function (req: Request, res: Response) {
+    const slug = req.params.slug;
+
+    if (!slug) {
+        res.status(403).json({ message: "Name of the room not provided." })
+        return
+    }
+
+    try {
+        const room = await prismaClient.room.findFirst({
+            where: {
+                slug: slug
+            }
+        });
+
+        if (!room) {
+            res.status(404).json({ message: "Room could not be found." })
+            return
         }
-    })
+
+        res.status(200).json({
+            roomId: room.id
+        })
+
+    } catch (e) {
+        res.status(403).json({
+            message: "Room with that name could not be found."
+        })
+    }
 })
 
 
