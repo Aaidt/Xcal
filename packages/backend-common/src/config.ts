f884ae91c8d383defa5517b101a08db8fcb33a5b
Aadit <141112198+Aaidt@users.noreply.github.com>
2025-06-11 11:47:51 +0530

    Delete packages/backend-common/src/config.ts

--- packages/backend-common/src/config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config()
-
-export const JWT_SECRET = process.env.JWT_SECRET ?? "ihaveacasiowatch"
-if(!JWT_SECRET){
-    console.log('jwt secret not provided')
-}
