fd0027da0a38b4f7520c9c7be3779694a6a5e276
Aadit <aadits2004@gmail.com>
2025-06-12 10:02:46 +0000

    roomId extraction

--- apps/xcal-frontend/app/canvas/[slug]/page.tsx
+++ apps/xcal-frontend/app/canvas/[slug]/page.tsx
@@ -1,75 +1,17 @@
-"use client"
+import GetRoomId from "@/components/GetRoomId"
 
-import axios from 'axios'
-import { toast } from "react-toastify"
-import ChatRoom from "@/components/ChatRoom"
-import { useEffect, useState } from 'react'
-
-interface axiosResponse {
-    roomId: number
-}
-
-async function getRoomId({
-    slug,
-    BACKEND_URL,
-    token
-}: {
-    slug: string,
-    BACKEND_URL: string,
-    token: string
-}) {
-    try {
-        const response = await axios.get<axiosResponse>(`${BACKEND_URL}/api/room/${slug}`, {
-            headers: {
-                "Authorization": token
-            }
-        })
-        return response?.data.roomId
-    } catch (err: unknown) {
-        if (axios.isAxiosError(err)) {
-            const errorMessage = err?.response?.data?.message
-            toast.error(errorMessage)
-        }
-    }
-}
-
-export default function Canvas({
+export default async function Canvas({
     params
 }: {
     params: {
         slug: string
     }
 }) {
-    const [roomId, setRoomId] = useState<number | null>(null);
-    const { slug } = params;
-    const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+    const { slug } = params
 
-    useEffect(() => {
-        async function fetchRoomId() {
-            if (!BACKEND_URL) {
-                toast.error('BACKEND_URL is not defined');
-                return;
-            }
-            const token = localStorage.getItem('Authorization');
-            if (!token) {
-                toast.error('Authorization token not found');
-                return;
-            }
-
-            const id = await getRoomId({ slug, BACKEND_URL, token });
-            if (!id) {
-                toast.error('Could not find the room');
-                return;
-            }
-            setRoomId(id);
-        }
-
-        fetchRoomId();
-    }, [slug, BACKEND_URL]);
-
-    if (!roomId) {
-        return null;
+    if(!slug){
+        return <div>Invalid room name...</div>
     }
 
-    return <ChatRoom roomId={roomId} />;
+    return <GetRoomId slug={slug} />
 }
---
+++ apps/xcal-frontend/components/RoomCanvas.tsx
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+// import { toast } from "react-toastify"
+
+export default async function RoomCanvas({ roomId }: { roomId: number }) {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
